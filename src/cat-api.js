79a479d5240a66a0a9456d8adb274928aa9ce832
SvetlanaDobro <122154093+SvetlanaDobro@users.noreply.github.com>
2023-06-04 21:10:17 +0300

    fix-1

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,36 +1,23 @@
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY = 'live_JnFRMOzGTO4ryjLp8pNYcImq7oeNXrAII0GkIylbiZplQgFxhVvgzE7RrR8fguia';
 
 export function fetchBreeds() {
-  return fetch('https://api.thecatapi.com/v1/breeds')
-    .then(response => response.json())
-    .then(data => data.map(breed => ({ id: breed.id, name: breed.name })))
-    .catch(error => {
-      console.error('Error fetching breeds:', error);
-      return [];
-    });
-}
-
+  return fetch(`${BASE_URL}/breeds`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      return response.json()
+    })
+};
 
 export function fetchCatByBreed(breedId) {
-  const BASE_URL = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=live_JnFRMOzGTO4ryjLp8pNYcImq7oeNXrAII0GkIylbiZplQgFxhVvgzE7RrR8fguia`;
-
-  return fetch(BASE_URL)
-    .then(response => response.json())
-    .then(data => {
-      if (data.length > 0) {
-        const cat = data[0];
-        return {
-          breed: cat.breeds[0].name,
-          description: cat.breeds[0].description,
-          temperament: cat.breeds[0].temperament,
-          image: cat.url
-        };
-      } else {
-        throw new Error('No cat data found');
+  return fetch(`${BASE_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
       }
+      return response.json()
     })
-    .catch(error => {
-      console.error('Error fetching cat:', error);
-      throw error;
-    });
-}
+};
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { fetchBreeds, fetchCatByBreed} from './cat-api';
 
 
-
 const breedSelect = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
@@ -11,82 +10,91 @@ hideError();
 showLoader();
 
 fetchBreeds()
-    .then(breeds => {
-        breeds.forEach(breed => {
-            const option = document.createElement('option');
-            option.value = breed.id;
-            option.textContent = breed.name;
-            breedSelect.appendChild(option);
-        });
-          
-    })
-    .catch(error => {    
-        showError();
-        console.error('Error fetching breeds:', error);
-    }).finally(() => {
-        hideLoader();
-    });
+  .then(mapBreedsToOptions)
+  .then(renderBreedOptions)
+  .catch(handleError)
+  .finally(hideLoader);
+
+  breedSelect.addEventListener('change', handleBreedSelectChange);
+ 
+function mapBreedsToOptions(breeds) {
+  return breeds.map(breed => `<option value="${breed.id}">${breed.name}</option>`);
+}
+
+function renderBreedOptions(breedOptions) {
+  breedSelect.innerHTML = breedOptions.join('');
+}
+
+function handleError(error) {
+  showError();
+  console.error('Error fetching breeds:', error);
+}
 
 
-breedSelect.addEventListener('change', () => {
-      const selectedBreedId = breedSelect.value;
+function handleBreedSelectChange() {
+  const selectedBreedId = breedSelect.value;
 
-    showLoader();
-    hideError();
-    hideCatInfo();
+  showLoader();
+  hideError();
+  hideCatInfo();
 
-    fetchCatByBreed(selectedBreedId)
-        .then(catData => {
-            catInfo.innerHTML = `
+  fetchCatByBreed(selectedBreedId)
+    .then(formatCatData)
+    .then(updateCatInfo)
+    .catch(handleCatError)
+    .finally(hideLoader);
+};
+
+function updateCatInfo(catData) {  
+  catInfo.innerHTML = `
             <img src="${catData.image}" alt="${catData.breed}">
             <h2>${catData.breed}</h2>
             <p><strong>Description:</strong> ${catData.description}</p>
             <p><strong>Temperament:</strong> ${catData.temperament}</p>
           `;
-            showCatInfo();
-        })
-        .catch(error => {          
-            showError();
-            console.error('Error fetching cat:', error);
-        }).finally(() => {
-            hideLoader();
-        });
-});
-    
+  showCatInfo();
+}
+
+function formatCatData(catData) {
+  if (catData.length > 0) {
+    const cat = catData[0];
+    return {
+      breed: cat.breeds[0].name,
+      description: cat.breeds[0].description,
+      temperament: cat.breeds[0].temperament,
+      image: cat.url
+    };
+  } else {
+    throw new Error('No cat data found');
+  }
+};
+
+function handleCatError(error) {
+  showError();
+  console.error('Error fetching cat:', error);
+}
+
 function showLoader() {
   loader.classList.add('active');
-}
+};
     
- function hideLoader() {
+function hideLoader() {
       loader.classList.remove('active');
-    
-    }
+};
 
-    function showError() {
+function showError() {
       error.classList.remove('hidden');
-}
+};
 
 function hideError() {
   error.classList.add('hidden');
-}
+};
 
 function showCatInfo() {
   catInfo.classList.remove('hidden');
-}
+};
 
 function hideCatInfo() {
   catInfo.classList.add('hidden');
-}
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
